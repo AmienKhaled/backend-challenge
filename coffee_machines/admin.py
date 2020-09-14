4b378a0ee6f7b465f0dfa24ff6a5d57e6ddb49c1
Amien <be._.positive@hotmail.com>
2020-09-15 01:34:13 +0200

    modify line length to pass from linter

--- coffee_machines/admin.py
+++ coffee_machines/admin.py
@@ -3,8 +3,12 @@ from coffee_machines import models
 
 
 class MachinesAdmin(admin.ModelAdmin):
-    """This class to overwrite CoffeeMachines models appearance in admin panel"""
-    list_display  = ('name', 'machine_type', 'machine_class', 'water_line_compatiple',)
+    """
+        This class to overwrite CoffeeMachines
+        models appearance in admin panel
+    """
+    list_display = ('name', 'machine_type',
+                    'machine_class', 'water_line_compatiple',)
 
 
 admin.site.register(models.CoffeeMachines, MachinesAdmin)
